ff21c4166021a39d54ab235aad639c6bc4b332ab
mbonum <mbonum@users.noreply.github.com>
2026-05-17 15:06:36 +0200

    style: match blog items design to job experiences

--- app/page.tsx
+++ app/page.tsx
@@ -2,7 +2,6 @@
 import { XIcon } from "lucide-react";
 import { motion } from "motion/react";
 import Link from "next/link";
-import { AnimatedBackground } from "@/components/ui/animated-background";
 import { Magnetic } from "@/components/ui/magnetic";
 import {
 	MorphingDialog,
@@ -173,35 +172,35 @@ export default function Personal() {
 ) : null}
 
 <motion.section variants={VARIANTS_SECTION} transition={TRANSITION_SECTION}>
-	<h3 className="mb-3 text-lg font-medium">Blog</h3>
-				<div className="flex flex-col space-y-0">
-					<AnimatedBackground
-						enableHover
-						className="h-full w-full rounded-lg bg-zinc-100 dark:bg-zinc-900/80"
-						transition={{
-							type: "spring",
-							bounce: 0,
-							duration: 0.2,
-						}}
-					>
-						{BLOG_POSTS.map((post) => (
-							<Link
-								key={post.uid}
-								className="-mx-3 rounded-xl px-3 py-3"
-								href={post.link}
-								data-id={post.uid}
-							>
-								<div className="flex flex-col space-y-1">
-									<h4 className="font-normal dark:text-zinc-100">
-										{post.title}
-									</h4>
-									<p className="text-zinc-500 dark:text-zinc-400">
-										{post.description}
+	<h3 className="mb-5 text-lg font-medium">Blog</h3>
+				<div className="flex flex-col space-y-2">
+					{BLOG_POSTS.map((post) => (
+						<Link
+							key={post.uid}
+							className="relative overflow-hidden rounded-2xl bg-zinc-300/30 p-[1px] dark:bg-zinc-600/30 block"
+							href={post.link}
+						>
+							<Spotlight
+								className="from-zinc-900 via-zinc-800 to-zinc-700 blur-2xl dark:from-zinc-100 dark:via-zinc-200 dark:to-zinc-50"
+								size={64}
+							/>
+							<div className="relative h-full w-full rounded-[15px] bg-white p-4 dark:bg-zinc-950">
+								<div className="relative flex w-full flex-row justify-between">
+									<div>
+										<h4 className="font-normal dark:text-zinc-100">
+											{post.title}
+										</h4>
+										<p className="text-zinc-500 dark:text-zinc-400 mt-1">
+											{post.description}
+										</p>
+									</div>
+									<p className="text-zinc-600 dark:text-zinc-400 whitespace-nowrap ml-4 mt-0.5">
+										{post.date}
 									</p>
 								</div>
-							</Link>
-						))}
-					</AnimatedBackground>
+							</div>
+						</Link>
+					))}
 				</div>
 			</motion.section>
 
